36491021479364f9727e0104d8927bfacbd9822d
Satish Chandra <satish.chandra@cashfree.com>
2025-04-10 18:00:11 +0530

    added deleted files

---
+++ go/echo/config/config.go
@@ -0,0 +1,31 @@
+package config
+
+import (
+    "github.com/joho/godotenv"
+    "log"
+    "os"
+)
+
+type Config struct {
+    Port string
+    Env  string
+}
+
+func LoadConfig() Config {
+    err := godotenv.Load()
+    if err != nil {
+        log.Fatal("Error loading .env file")
+    }
+
+    return Config{
+        Port: getEnv("PORT", "8080"),
+        Env:  getEnv("ENV", "development"),
+    }
+}
+
+func getEnv(key, defaultValue string) string {
+    if value, exists := os.LookupEnv(key); exists {
+        return value
+    }
+    return defaultValue
+}
