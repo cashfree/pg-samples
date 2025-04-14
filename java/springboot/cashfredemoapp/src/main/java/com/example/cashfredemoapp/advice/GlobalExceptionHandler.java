1c055414fbe787bb416a78c5b057709b03aa2505
Shubhendra Singh Chauhan <withshubh@gmail.com>
2025-04-14 15:35:09 +0530

    added comments in springboot

--- java/springboot/cashfredemoapp/src/main/java/com/example/cashfredemoapp/advice/GlobalExceptionHandler.java
+++ java/springboot/cashfredemoapp/src/main/java/com/example/cashfredemoapp/advice/GlobalExceptionHandler.java
@@ -5,11 +5,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+// Global exception handler to handle exceptions across the application
 @RestControllerAdvice
-    public class GlobalExceptionHandler {
-        @ExceptionHandler(Exception.class)
-        public ResponseEntity<?> handleAll(Exception e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
-        }
+public class GlobalExceptionHandler {
+
+    // Handles all exceptions and returns a BAD_REQUEST response with the error message
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<?> handleAll(Exception e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
     }
+}
 
