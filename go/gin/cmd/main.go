eae4c5e1996f54021208fbcf6d24955ad277fe63
Shubhendra Singh Chauhan <withshubh@gmail.com>
2025-04-14 15:30:33 +0530

    comments in gin

--- go/gin/cmd/main.go
+++ go/gin/cmd/main.go
@@ -8,13 +8,19 @@ import (
 )
 
 func main() {
+	// Setup the Gin router
 	r := routes.SetupRouter()
+
+	// Load HTML templates for rendering
 	r.LoadHTMLGlob("internal/templates/*")
+
+	// Load environment variables from .env file
 	err := godotenv.Load(".env")
 	if err != nil {
+		// Log error if .env file is missing or cannot be loaded
 		log.Fatalf("Error loading .env file: %v", err)
 	}
 
-	routes.SetupRouter()
+	// Start the server on port 8080
 	r.Run(":8080")
 }
--- go/gin/internal/routes/router.go
+++ go/gin/internal/routes/router.go
@@ -8,9 +8,18 @@ import (
 
 func SetupRouter() *gin.Engine {
 	router := gin.Default()
+
+	// Route to serve the home page
 	router.GET("/", handlers.HomeHandler)
+
+	// Route to create an order and generate a payment session ID
 	router.POST("/create-order", handlers.CreateOrderHandler)
+
+	// Route to fetch and display the order status
 	router.GET("/status", handlers.StatusPageHandler)
+
+	// Route to handle webhook notifications from Cashfree
 	router.POST("/webhook", handlers.WebhookHandler)
+
 	return router
 }
